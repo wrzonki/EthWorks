f8aec057d0b609fb7a2c2d1ba8a5db36286e56d7
wrzonki <wrzonki@gmail.com>
2019-11-02 00:57:35 +0100

    init

---
+++ js/clock.js
@@ -0,0 +1,17 @@
+var countDownDate = new Date("Jan 5, 2018 15:37:25").getTime();
+
+var x = setInterval(function() {
+  var now = new Date().getTime();
+  var distance = countDownDate - now;
+
+  var days = Math.floor(distance / (1000 * 60 * 60 * 24));
+  var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+  var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+  var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+
+  document.getElementById("days").innerHTML = days + "d ";
+  document.getElementById("hours").innerHTML = hours + "h ";
+  document.getElementById("minutes").innerHTML = minutes + "m ";
+  document.getElementById("seconds").innerHTML = seconds + "s ";
+
+}, 1000);
